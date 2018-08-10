eac3bedd0e55b3dcd6860c68a22e0317e878b016
Yi Le <leyi@frugalops.com>
2018-08-10 15:39:51 -0400

    TokenGenerator and config added

--- cognito/TokenGenerator.js
+++ cognito/TokenGenerator.js
@@ -1,23 +1,25 @@
 var AmazonCognitoIdentity = require('amazon-cognito-identity-js');
 var AWS =require('aws-sdk');
+var config =require('./config.js')
+
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
 
 var authenticationData = {
-    Username : process.argv[2],
-    Password : process.argv[3],
+    Username : config.Username,
+    Password : config.Password,
 };
 
 var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
 
 var poolData = {
-    UserPoolId : 'us-east-1_N8lGIMwK0', // Your user pool id here
-    ClientId : '1keu7b6ejucnj3bj934p604rmq' // Your client id here
+    UserPoolId : config.UserPoolId, // Your user pool id here
+    ClientId : config.ClientId // Your client id here
 };
 
 var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
 var userData = {
-    Username : process.argv[2],
+    Username : config.Username,
     Pool : userPool
 };
 
@@ -25,13 +27,16 @@ var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 cognitoUser.authenticateUser(authenticationDetails, {
     onSuccess: function (result) {
         var accessToken = result.getAccessToken().getJwtToken();
-        AWS.config.region = 'us-east-1';
+        AWS.config.region = config.Region;
+
+        var cognito = '"' + 'cognito-idp.' + config.Region + '.amazonaws.com/' + config.UserPoolId + '"';
+        var text = '{' + cognito + ':' + '"' + result.getIdToken().getJwtToken() + '"' + '}';
+
+        var obj = JSON.parse(text);
 
         AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-            IdentityPoolId : 'us-east-1:33fe17ca-ec30-45fa-b1fa-08040714a366',
-            Logins : {
-                'cognito-idp.us-east-1.amazonaws.com/us-east-1_N8lGIMwK0' : result.getIdToken().getJwtToken()
-            }
+            IdentityPoolId : config.IdentityPoolId,
+            Logins : obj
         });
 
     
--- cognito/config.js
+++ cognito/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-	UserPoolId : 'us-east-1_l8lq8IwZJ', // Your user pool id here
-	ClientId : '5vj3l36ms976p3i42svtmpnkr8', // Your client id here
-	Username: 'frugalops', // Your Username here
-	Password: 'Frugalops2018!', // Your Password here
-	IdentityPoolId: 'us-east-1:b765e3b4-2ed1-4968-898d-3d209e6a544a', // Your IdentityPoolId here
-	Region: 'us-east-1' // Your AWS Region here
+	UserPoolId : '${UserPoolId}', // Your user pool id here
+	ClientId : '${ClientId}', // Your client id here
+	Username: '${Username}', // Your Username here
+	Password: '${Password}', // Your Password here
+	IdentityPoolId: '${IdentityPoolId}', // Your IdentityPoolId here
+	Region: '${AWS_REGION}' // Your AWS Region here
 }
